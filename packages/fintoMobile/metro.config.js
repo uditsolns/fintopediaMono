0e31b0a01b56de72bd37d786f34f1085f6a03b53
Ankit Singh <aks.11feb@gmail.com>
2024-07-22 20:14:00 +0530

    changes in pods, modules and structure

--- packages/fintoMobile/metro.config.js
+++ packages/fintoMobile/metro.config.js
@@ -1,11 +1,37 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+const defaultSourceExts =
+  require('metro-config/src/defaults/defaults').sourceExts;
+const defaultAssetExts =
+  require('metro-config/src/defaults/defaults').assetExts;
+
+const path = require('path');
+
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    extraNodeModules: {
+      shared: path.resolve(__dirname, '../shared'),
+    },
+    sourceExts: [...defaultSourceExts, 'native.tsx', 'web.tsx'],
+  },
+  watchFolders: [
+    path.resolve(__dirname, '../../node_modules'),
+    path.resolve(__dirname, '../../node_modules/shared'),
+  ],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
