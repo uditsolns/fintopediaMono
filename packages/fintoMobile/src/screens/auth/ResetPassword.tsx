b6d3b53f432f1aac94ef6bbfc7a487ef045fe7d0
Sujeet Chauhan <sujeetchauhan956@gmail.com>
2024-09-24 18:41:11 +0530

    -forgot api integration

--- packages/fintoMobile/src/screens/auth/ResetPassword.tsx
+++ packages/fintoMobile/src/screens/auth/ResetPassword.tsx
@@ -1,32 +1,44 @@
-import {useNavigation} from '@react-navigation/native';
 import {Images} from '@shared/src/assets';
 import {ButtonAtom} from '@shared/src/components/atoms/Button/ButtonAtom';
-import ImageAtom from '@shared/src/components/atoms/Image/ImageAtom';
 import ScrollViewAtom from '@shared/src/components/atoms/ScrollView/ScrollViewAtom';
 import {TextAtom} from '@shared/src/components/atoms/Text/TextAtom';
 import {GradientTemplate} from '@shared/src/components/templates/GradientTemplate';
+import {useAppSelector} from '@shared/src/provider/store/types/storeTypes';
 import {colorPresets} from '@shared/src/theme/color';
 import {mScale} from '@shared/src/theme/metrics';
-import {LinkButton} from '@src/components/Button/LinkButton';
 import HeaderLeftMolecule from '@src/components/Header/HeaderLeftMolecule';
 import {InputAtom} from '@src/components/Input/InputAtom';
 import * as React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
+import {useResetHelper} from '@shared/src/components/structures/reset/reset.helper';
+import {resetField} from '@shared/src/components/structures/reset/resetModel';
+import {NavType} from '@src/navigation/types';
+import {PressableAtom} from '@shared/src/components/atoms/Button/PressableAtom';
 
-interface ResetPasswordProps {}
+interface ResetPasswordProps extends NavType<'ResetPassword'> {}
 
-export const ResetPassword: React.FC<ResetPasswordProps> = ({}) => {
-  const navigation = useNavigation();
+export const ResetPassword: React.FC<ResetPasswordProps> = ({navigation}) => {
+  const {confirm, loading} = useAppSelector(state => state.auth);
+  console.log(confirm)
+  const {resetFormik, resetInputProps} = useResetHelper();
+  const {handleSubmit, setFieldValue} = resetFormik;
+  const [passwordVisible, setPasswordVisible] = React.useState<boolean>(true);
+
+  const togglePassword = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  React.useEffect(() => {
+    if (confirm) {
+    }
+  }, [confirm]);
   return (
     <GradientTemplate>
-      <HeaderLeftMolecule />
-
-      <ScrollViewAtom>
+      <ScrollViewAtom contentContainerStyle={{marginTop: mScale.xxl1}}>
         <View>
           <View style={{marginVertical: mScale.base, padding: mScale.md}}>
             <View style={{alignSelf: 'center'}}>
               <Images.SVG.ForgotIcon />
-              {/* <ImageAtom sourceRequire={require('@shared/src/assets/img/lockIcon.png')} /> */}
             </View>
             <TextAtom
               text={`Reset Password?`}
@@ -42,22 +54,48 @@ export const ResetPassword: React.FC<ResetPasswordProps> = ({}) => {
           <View style={{marginBottom: mScale.lg}}>
             <InputAtom
               shape="square"
-              label="New Password"
-              placeholder="Enter your new password"
-              rightIcon={<Images.SVG.Eye width={20} color={colorPresets.CTA} />}
+              {...resetInputProps(resetField.password.name)}
+              label={resetField.password.label}
+              placeholder={resetField.password.placeHolder}
+              rightIcon={
+                <PressableAtom onPress={togglePassword}>
+                  {passwordVisible ? (
+                    <Images.SVG.Eye width={20} color={colorPresets.CTA} />
+                  ) : (
+                    <Images.SVG.EyeOff width={20} color={colorPresets.CTA} />
+                  )}
+                </PressableAtom>
+              }
+              secureTextEntry={passwordVisible ? true : false}
               autoCapitalize="none"
             />
           </View>
           <View style={{marginBottom: mScale.lg}}>
             <InputAtom
               shape="square"
-              label="Confirm Password"
-              placeholder="Enter your confirm password"
-              rightIcon={<Images.SVG.Eye width={20} color={colorPresets.CTA} />}
+              {...resetInputProps(resetField.password_confirmation.name)}
+              label={resetField.password_confirmation.label}
+              placeholder={resetField.password_confirmation.placeHolder}
+              rightIcon={
+                <PressableAtom onPress={togglePassword}>
+                  {passwordVisible ? (
+                    <Images.SVG.Eye width={20} color={colorPresets.CTA} />
+                  ) : (
+                    <Images.SVG.EyeOff width={20} color={colorPresets.CTA} />
+                  )}
+                </PressableAtom>
+              }
+              secureTextEntry={passwordVisible ? true : false}
               autoCapitalize="none"
             />
           </View>
-          <ButtonAtom title="Reset Password" />
+          <ButtonAtom
+            title="Reset Password"
+            onPress={() => {
+              handleSubmit();
+            }}
+            loading={loading?.confirm}
+          />
         </View>
       </ScrollViewAtom>
     </GradientTemplate>
