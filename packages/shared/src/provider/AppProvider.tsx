f5e80159a4281799de38b06a0099d610fac8f6c6
Ankit Singh <aks.11feb@gmail.com>
2025-09-20 14:49:07 +0530

    fix

--- packages/shared/src/provider/AppProvider.tsx
+++ packages/shared/src/provider/AppProvider.tsx
@@ -4,9 +4,6 @@ import * as React from "react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./store/store";
-import { ToastProvider } from "react-native-toast-notifications";
-import { ToastMolecule } from "../components/molecules/Toast/ToastMolecule";
-import { ToastProps } from "react-native-toast-notifications/lib/typescript/toast";
 import { Platform } from "react-native";
 import { ToastContainer } from "react-toastify";
 interface AppProviderProps {
@@ -23,20 +20,20 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
         <ToastContainer />
       </Provider>
     );
-  } else {
-    return (
-      <Provider store={store}>
-        <PersistGate persistor={persistor} loading={null}>
-          <ToastProvider
-            placement="bottom"
-            offsetTop={80}
-            offsetBottom={40}
-            renderToast={(toast: ToastProps) => <ToastMolecule {...toast} />}
-          >
-            {children}
-          </ToastProvider>
-        </PersistGate>
-      </Provider>
-    );
+    // } else {
+    //   return (
+    //     <Provider store={store}>
+    //       <PersistGate persistor={persistor} loading={null}>
+    //         <ToastProvider
+    //           placement="bottom"
+    //           offsetTop={80}
+    //           offsetBottom={40}
+    //           renderToast={(toast: ToastProps) => <ToastMolecule {...toast} />}
+    //         >
+    //           {children}
+    //         </ToastProvider>
+    //       </PersistGate>
+    //     </Provider>
+    //   );
   }
 };
